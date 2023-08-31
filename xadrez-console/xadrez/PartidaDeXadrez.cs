45e2dc949d5f720cef0d5359cb5a795b7ae2ecc3
Jaime Santiago <jaimesantiago543@gmail.com>
2023-08-31 10:05:45 -0300

    Jogada especial promocao

--- xadrez-console/xadrez/PartidaDeXadrez.cs
+++ xadrez-console/xadrez/PartidaDeXadrez.cs
@@ -45,7 +45,7 @@ namespace xadrez
             {
                 Posicao origemT = new Posicao(origem.linha, origem.coluna + 3);
                 Posicao destinoT = new Posicao(origem.linha, origem.coluna + 1);
-                
+
                 Peca T = tab.retirarPeca(origemT);
 
                 T.incrementarQteMovimentos();
@@ -165,6 +165,25 @@ namespace xadrez
                 throw new TabuleiroException("Você não pode se colocar em xeque!");
             }
 
+            Peca p = tab.peca(destino);
+
+            // #jogadaespecial promocao
+            if (p is Peao)
+            {
+                if ((p.cor == Cor.Branca && destino.linha == 0) || (p.cor == Cor.Preta && destino.linha == 7))
+                {
+                    p = tab.retirarPeca(destino);
+
+                    pecas.Remove(p);
+
+                    Peca dama = new Dama(tab, p.cor);
+
+                    tab.colocarPeca(dama, destino);
+
+                    pecas.Add(dama);
+                }
+            }
+
             if (estaEmXeque(adversaria(jogadorAtual)))
             {
                 xeque = true;
@@ -184,8 +203,6 @@ namespace xadrez
 
                 mudaJogador();
             }
-
-            Peca p = tab.peca(destino);
 
             // #jogadaespecial en passant
             if (p is Peao && (destino.linha == origem.linha - 2 || destino.linha == origem.linha + 2))
